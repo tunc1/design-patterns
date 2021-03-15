124ddc730c77a81a461a5610ffbdcff9dc77bc9c
User <user>
2021-03-15 03:34:36 +0300

    Modified abstract_factory_test

--- test/abstract_factory_test.py
+++ test/abstract_factory_test.py
@@ -1,12 +1,16 @@
 from unittest import TestCase
 from abstract_factory import *
 
-class AbstractFactoryTest(TestCase):
+class PlayerShipFactoryTest(TestCase):
     
     def test_create(self):
         factory=PlayerShipFactory()
         self.assertEqual(type(factory.create("normal")),PlayerNormalShip)
         self.assertEqual(type(factory.create("weaponed")),PlayerWeaponedShip)
+
+class EnemyShipFactoryTest(TestCase):
+    
+    def test_create(self):
         factory=EnemyShipFactory()
         self.assertEqual(type(factory.create("normal")),EnemyNormalShip)
         self.assertEqual(type(factory.create("weaponed")),EnemyWeaponedShip)
