1aa22fa5c47b6303286db3a8b731c71bd3a8dae1
User <user>
2021-03-27 08:48:29 +0300

    Modified Template Method Test

--- test/template_method_test.py
+++ test/template_method_test.py
@@ -3,18 +3,18 @@ from template_method import GithubSignIn,TwitterSignIn
 
 class GithubSignInTest(TestCase):
 
-    def test_sign_in(self):
+    def test_check(self):
         sign_in=GithubSignIn()
-        self.assertTrue(sign_in.sign_in("github","password"))
-        self.assertFalse(sign_in.sign_in("github","pass"))
-        self.assertFalse(sign_in.sign_in("git","password"))
-        self.assertFalse(sign_in.sign_in("git","passwor"))
+        self.assertTrue(sign_in.check("github","password"))
+        self.assertFalse(sign_in.check("github","pass"))
+        self.assertFalse(sign_in.check("git","password"))
+        self.assertFalse(sign_in.check("git","passwor"))
 
 class TwitterSignInTest(TestCase):
 
-    def test_sign_in(self):
+    def test_check(self):
         sign_in=TwitterSignIn()
-        self.assertTrue(sign_in.sign_in("twitter","password"))
-        self.assertFalse(sign_in.sign_in("twitter","pass"))
-        self.assertFalse(sign_in.sign_in("tt","password"))
-        self.assertFalse(sign_in.sign_in("tt","passwor"))
+        self.assertTrue(sign_in.check("twitter","password"))
+        self.assertFalse(sign_in.check("twitter","pass"))
+        self.assertFalse(sign_in.check("tt","password"))
+        self.assertFalse(sign_in.check("tt","passwor"))
