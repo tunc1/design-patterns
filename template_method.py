b383e26e985be05307562b4ef01629c06a20b914
User <user>
2021-03-27 08:23:39 +0300

    Revert "Added Template Method Test"
    This reverts commit 8d0ac237f528ad9e679df48172501ac4e5fbc09c.

--- template_method.py
+++ template_method.py
@@ -3,11 +3,19 @@ import getpass
 
 class SignIn(ABC):
     
-    def sign_in(self,username,password):
+    def sign_in(self):
+        username=self.get_username()
+        password=self.get_password()
         if self.check(username,password):
             return True
         else:
             return False
+    
+    def get_username(self):
+        return input("Enter username: ")
+    
+    def get_password(self):
+        return getpass.getpass("Enter password: ")
     
     @abstractmethod
     def check(self,username,password):
