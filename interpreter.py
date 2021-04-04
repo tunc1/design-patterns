a7ac694430604d0a2b7fffc2b86c5ae84d2097b2
User <user>
2021-04-05 01:47:51 +0300

    Modified Interpreter

--- interpreter.py
+++ interpreter.py
@@ -6,79 +6,21 @@ class Expression(ABC):
     def interpret(self,context):
         pass
 
-    def __init__(self,context,symbol):
-        index=context.index(symbol)
-        self.left_expression=NonTerminalExpression(context[:index])
-        self.right_expression=NonTerminalExpression(context[index+1:])
+class TerminalExpression(Expression):
 
-class NumberExpression(Expression):
-    
-    def __init__(self,context):
-        self.context=context
-
-    def interpret(self):
-        return int(self.context)
-
-class MultiplyExpression(Expression):
-    
-    def __init__(self,context):
-        super().__init__(context,"*")
-
-    def interpret(self):
-        return self.left_expression.interpret()*self.right_expression.interpret()
-
-class PlusExpression(Expression):
-    
-    def __init__(self,context):
-        super().__init__(context,"+")
-
-    def interpret(self):
-        return self.left_expression.interpret()+self.right_expression.interpret()
-
-class DivideExpression(Expression):
-    
-    def __init__(self,context):
-        super().__init__(context,"/")
-
-    def interpret(self):
-        return self.left_expression.interpret()/self.right_expression.interpret()
-
-class MinusExpression(Expression):
-    
-    def __init__(self,context):
-        index=context[1:].index("-")+1
-        self.left_expression=NonTerminalExpression(context[:index])
-        self.right_expression=NonTerminalExpression("-"+context[index+1:])
-
-    def interpret(self):
-        return self.left_expression.interpret()+self.right_expression.interpret()
+    def interpret(self,context):
+        return int(context)
 
 class NonTerminalExpression(Expression):
 
-    def __init__(self,context):
-        self.context=context
-
-    def interpret(self):
-        symbol=self.get_first_symbol()
-        if "+"==symbol:
-            return PlusExpression(self.context).interpret()
-        elif "-"==symbol:
-            return MinusExpression(self.context).interpret()
-        elif "/"==symbol:
-            return DivideExpression(self.context).interpret()
-        elif "*"==symbol:
-            return MultiplyExpression(self.context).interpret()
+    def interpret(self,context):
+        index=context.find("+")
+        if index==-1:
+            terminal_expression=TerminalExpression()
+            return terminal_expression.interpret(context)
         else:
-            return NumberExpression(self.context).interpret()
-    
-    def get_first_symbol(self):
-        for i,char in enumerate(self.context):
-            if char=="+" or char=="-":
-                if i!=0:
-                    return char
-        for char in self.context:
-            if char=="*":
-                return char
-        for char in self.context:
-            if char=="/":
-                return char
+            number=context[:index]
+            rest=context[index+1:]
+            terminal_expression=TerminalExpression()
+            non_terminal_expression=NonTerminalExpression()
+            return terminal_expression.interpret(number)+non_terminal_expression.interpret(rest)
--- test/interpreter_test.py
+++ test/interpreter_test.py
@@ -4,5 +4,5 @@ from interpreter import NonTerminalExpression
 class InterpreterTest(TestCase):
     
     def test_interpret(self):
-        expression=NonTerminalExpression("-4+3*2-1+25/5-2+1*5-3/3+4/2*2")
-        self.assertEqual(expression.interpret(),12)
+        expression=NonTerminalExpression()
+        self.assertEqual(expression.interpret("3+1+5+2"),11)
