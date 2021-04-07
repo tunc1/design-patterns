ca8faa8ecc9f731f0ca81263a1945d695155cfe2
User <user>
2021-04-07 06:02:14 +0300

    Modified Facade

--- facade.py
+++ facade.py
@@ -1,20 +1,22 @@
-class WordReader:
-    
-    def read(self,file):
-        return "content"
+class Computer:
 
-class PDFWriter:
-    
-    def write(self,content,file):
-        print("Writing to",file)
-
-class WordPDFConverter:
-    
     def __init__(self):
-        self.word_reader=WordReader()
-        self.pdf_writer=PDFWriter()
+        self.cpu=CPU()
+        self.hdd=HDD()
     
-    def convert(self,word,pdf):
-        content=self.word_reader.read(word)
-        print("Reading from",word)
-        self.pdf_writer.write(content,pdf)
+    def turn_on(self):
+        self.cpu.has_electricity=True
+        self.hdd.start_turning()
+
+class CPU:
+
+    def __init__(self):
+        self.has_electricity=False
+
+class HDD:
+
+    def __init__(self):
+        self.is_turning=False
+    
+    def start_turning(self):
+        self.is_turning=True
--- test/facade_test.py
+++ test/facade_test.py
@@ -1,34 +1,10 @@
 from unittest import TestCase
-from facade import PDFWriter, WordPDFConverter, WordReader
-from io import StringIO
-import sys
+from facade import Computer
 
-class WordPDFConverterTest(TestCase):
+class ComputerTest(TestCase):
 
-    def test_convert(self):
-        stringIO=StringIO()
-        sys.stdout=stringIO
-        wordPDFConverter=WordPDFConverter()
-        word_file="file.docx"
-        pdf_file="file.pdf"
-        wordPDFConverter.convert(word_file,pdf_file)
-        self.assertEquals(stringIO.getvalue(),"Reading from "+word_file+"\nWriting to "+pdf_file+"\n")
-
-class WordReaderTest(TestCase):
-
-    def test_read(self):
-        stringIO=StringIO()
-        sys.stdout=stringIO
-        wordReader=WordReader()
-        file="file.docx"
-        self.assertEquals(wordReader.read(file),"content")
-
-class PDFWriterTest(TestCase):
-
-    def test_write(self):
-        stringIO=StringIO()
-        sys.stdout=stringIO
-        pdfWriter=PDFWriter()
-        file="file.pdf"
-        pdfWriter.write("content",file)
-        self.assertEquals(stringIO.getvalue(),"Writing to "+file+"\n")
+    def test_turn_on(self):
+        computer=Computer()
+        computer.turn_on()
+        self.assertTrue(computer.cpu.has_electricity)
+        self.assertTrue(computer.hdd.is_turning)
